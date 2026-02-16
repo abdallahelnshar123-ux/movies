a932179f39ab049dc4be958f58c37375c747f32b
mhmed <mhmed.wagdy2002@gmail.com>
2026-02-16 08:36:23 +0200

    onBoardingScreen_firstCommit

---
+++ lib/onboarding_screen/CustomButton.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:movies/utils/app_colors.dart';
+import 'package:movies/utils/app_styles.dart';
+import 'package:movies/utils/screen_size.dart';
+
+class CustomButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final bool isOutlined;
+
+  const CustomButtonWidget({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isOutlined = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width:context.width,
+      height: context.height * 0.06,
+      child: isOutlined
+          ? ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.transparentColor,
+          side: BorderSide(color: AppColors.yellowColor,width: 2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: AppStyles.interSBold20yellow
+        ),
+      )
+          : ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.amber,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: AppStyles.interSBold20Black
+        ),
+      ),
+    );
+  }
+}
