d28065bba9f4bce4e68f07ef856f901c52b89e79
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-02-17 23:24:58 +0200

    ..

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:movies/utils/app_colors.dart';
 
-import 'app_colors.dart';
 import 'app_styles.dart';
 
 class AppTheme {
   static ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: AppColors.blackColor,
-    appBarTheme: AppBarThemeData(
-      backgroundColor: AppColors.blackColor
-  ),
-    bottomSheetTheme: BottomSheetThemeData(
-      backgroundColor: Colors.transparent
-    )
-  );
-  static ThemeData darkTheme = ThemeData(
-      scaffoldBackgroundColor: AppColors.blackColor,
-      appBarTheme: AppBarTheme(
-        backgroundColor: AppColors.blackColor,
-        elevation: 0,
-        centerTitle: true,
-        iconTheme: IconThemeData(
-          color: AppColors.yellowColor,
-        ),
-        titleTextStyle: AppStyles.robotoRegular16Yellow,
-      )
-
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.blackColor,
+      elevation: 0,
+      centerTitle: true,
+      iconTheme: IconThemeData(color: AppColors.yellowColor),
+      titleTextStyle: AppStyles.robotoRegular16Yellow,
+    ),
+    bottomSheetTheme: BottomSheetThemeData(backgroundColor: Colors.transparent),
   );
 }
