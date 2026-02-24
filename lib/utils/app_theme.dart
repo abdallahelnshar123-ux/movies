82712718f4a2d445fbeca33ea815a6fd2a656c14
AbdelrhmanElhussien <abhussien240@gmail.com>
2026-02-24 18:44:49 +0200

    edit in Widget Custome_button edit in Update_Profile_screen

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -14,17 +14,5 @@ class AppTheme {
       backgroundColor: Colors.transparent
     )
   );
-  static ThemeData darkTheme = ThemeData(
-      scaffoldBackgroundColor: AppColors.blackColor,
-      appBarTheme: AppBarTheme(
-        backgroundColor: AppColors.blackColor,
-        elevation: 0,
-        centerTitle: true,
-        iconTheme: IconThemeData(
-          color: AppColors.yellowColor,
-        ),
-        titleTextStyle: AppStyles.robotoRegular16Yellow,
-      )
 
-  );
 }
